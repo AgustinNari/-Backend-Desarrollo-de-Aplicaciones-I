41bb195ffda4a5309ee7bf71225522db54a4a6e0
AgustinNari <agusnari21@gmail.com>
2026-05-06 10:53:30 -0300

    Setup Spring

---
+++ quickbid/src/test/java/com/example/quickbid/quickbid/QuickbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickbid.quickbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickbidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
